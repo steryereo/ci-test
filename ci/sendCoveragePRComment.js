1e7d3986aa6ecbc5505b44606763a82b7596532b
Nick Echols <nicke@scribd.com>
2022-12-01 17:18:15 -0800

    feat: lots of progress

--- ci/sendCoveragePRComment.js
+++ ci/sendCoveragePRComment.js
@@ -1,74 +1,101 @@
-const fetch = require("node-fetch")
+const { Octokit } = require("@octokit/core");
 
-const fs = require("fs")
-require("dotenv").config()
+const fs = require("fs");
 
-const TEST_COVERAGE_THRESHOLD = process.env.TEST_COVERAGE_THRESHOLD
+const TEST_COVERAGE_THRESHOLD = process.env.TEST_COVERAGE_THRESHOLD;
 
-const KEYS = ["statements", "branches", "functions", "lines"]
+const KEYS = ["statements", "branches", "functions", "lines"];
 
-function getPercentCell(entry) {
-  return `${entry.pct}% ${entry.pct >= TEST_COVERAGE_THRESHOLD ? "✅" : "❌"}`
+function getPercentCell(baseEntry, headEntry) {
+  return `~~${baseEntry.pct}%~~ ${headEntry.pct} ${
+    headEntry.pct >= baseEntry.pct ? "✅" : "❌"
+  }`;
 }
 
 function getRow(cells) {
-  return `| ${cells.join(" | ")} |`
+  return `| ${cells.join(" | ")} |`;
 }
 
-function generateCoverageCommentData(file) {
-  if (!fs.existsSync(file)) throw new Error(`file not found: ${file}`)
+function generateCoverageCommentData(baseReportFile, headReportFile) {
+  const baseReportPath = `${__dirname}/${baseReportFile}`;
+  const headReportPath = `${__dirname}/${headReportFile}`;
 
-  const data = JSON.parse(fs.readFileSync(file))
+  if (!fs.existsSync(baseReportPath))
+    throw new Error(`file not found: ${baseReportPath}`);
+  if (!fs.existsSync(headReportPath))
+    throw new Error(`file not found: ${headReportPath}`);
+
+  const baseData = JSON.parse(fs.readFileSync(baseReportPath));
+  const headData = JSON.parse(fs.readFileSync(headReportPath));
 
   const rows = [
     "### Test coverage",
     getRow(KEYS),
     getRow(KEYS.map(() => "---")),
-    getRow(KEYS.map((key) => getPercentCell(data.total[key]))),
-  ]
+    getRow(
+      KEYS.map((key) =>
+        getPercentCell(baseData.total[key], headData.total[key])
+      )
+    ),
+  ];
 
-  const body = rows.join("\n")
-
-  return JSON.stringify({ body })
+  return rows.join("\n");
 }
 
-function makeRequest(body, prNumber, githubToken) {
-  const authToken = Buffer.from(`scribdbot:${githubToken}`).toString("base64")
+async function makeRequest(body, prNumber, githubToken) {
+  const octokit = new Octokit({ auth: githubToken });
 
-  return fetch(`https://api.github.com/repos/scribd/node-chassis/issues/${prNumber}/comments`, {
-    method: "POST",
-    headers: {
-      Accept: "application/vnd.github+json",
-      Authorization: `Basic ${authToken}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body,
-  })
+  await octokit.request(
+    `POST /repos/steryereo/ci-test/issues/${prNumber}/comments`,
+    {
+      owner: "steryereo",
+      repo: "ci-test",
+      issue_number: prNumber,
+      body,
+    }
+  );
+  // return fetch(
+  //   `https://api.github.com/repos/scribd/node-chassis/issues/${prNumber}/comments`,
+  //   {
+  //     method: "POST",
+  //     headers: {
+  //       Accept: "application/vnd.github+json",
+  //       Authorization: `Basic ${authToken}`,
+  //       "Content-Type": "application/x-www-form-urlencoded",
+  //     },
+  //     body,
+  //   }
+  // );
 }
 
-async function sendCoveragePRComment(file, prNumber, githubToken) {
+async function sendCoveragePRComment(
+  baseReportFile,
+  headReportFile,
+  prNumber,
+  githubToken
+) {
   try {
-    const body = generateCoverageCommentData(file)
+    const body = generateCoverageCommentData(baseReportFile, headReportFile);
 
-    const response = await makeRequest(body, prNumber, githubToken)
+    const response = await makeRequest(body, prNumber, githubToken);
 
-    console.log(JSON.stringify({ file, prNumber, githubToken }))
+    console.log(JSON.stringify({ file, prNumber, githubToken }));
 
-    const data = await response.json()
+    const data = await response.json();
 
-    console.log(data)
-    if (response.status !== 201) throw new Error(response.statusText)
+    console.log(data);
+    if (response.status !== 201) throw new Error(response.statusText);
 
-    console.log(`Sent comment to PR ${data.html_url}`)
+    console.log(`Sent comment to PR ${data.html_url}`);
   } catch (e) {
-    console.error(e)
-    process.exitCode = 1
+    console.error(e);
+    process.exitCode = 1;
   }
 }
 
 /* istanbul ignore next */
 if (require.main === module) {
-  sendCoveragePRComment(...process.argv.slice(2))
+  sendCoveragePRComment(...process.argv.slice(2));
 }
 
-module.exports = sendCoveragePRComment
+module.exports = sendCoveragePRComment;
